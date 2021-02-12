5ef52e969129f89bd2dd1eabff5b07f4336e82aa
KalacskaB <kalacska1010@gmail.com>
2021-02-12 22:13:49 +0100

    Elso feladat

--- src/teszt/Teszt.java
+++ src/teszt/Teszt.java
@@ -15,7 +15,7 @@ public class Teszt {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Első feladat");
     }
     
 }
