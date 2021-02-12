5b79e08dc9de878d7e96531dea7f272c1c69f7f7
KalacskaB <kalacska1010@gmail.com>
2021-02-12 22:14:45 +0100

    Masodik feladat

--- src/teszt/Teszt.java
+++ src/teszt/Teszt.java
@@ -16,6 +16,10 @@ public class Teszt {
      */
     public static void main(String[] args) {
         System.out.println("Első feladat");
+        System.out.println("Első feladat");
+        System.out.println("Első feladat");
+        System.out.println("Első feladat");
+        System.out.println("Első feladat");
     }
     
 }
